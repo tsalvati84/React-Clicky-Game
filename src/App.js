17e6198380e5abd4e9e93e3129f48e3791f0a0c3
Tony Salvati <tsal2184@gmail.com>
2018-12-19 12:34:00 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MatchCard from "./components/Matchcard";
+import matches from "./matchcards.json";
+import Wrapper from "./components/Wrapper";
 import './App.css';
 
+let correctGuesses = 0;
+let bestScore = 0;
+
 class App extends Component {
+
+  state = {
+    matches,
+    correctGuesses,
+    bestScore,
+
+  };
+
+  setClicked = id => {
+
+
+    const matches = this.state.matches;
+
+
+    const clickedMatch = matches.filter(match => match.id === id);
+
+
+    if (clickedMatch[0].clicked) {
+
+      console.log("Correct Guesses: " + correctGuesses);
+      console.log("Best Score: " + bestScore);
+
+      correctGuesses = 0;
+      
+
+      for (let i = 0; i < matches.length; i++) {
+        matches[i].clicked = false;
+      }
+      this.setState({ correctGuesses });
+      this.setState({ matches });
+
+
+    } else if (correctGuesses < 11) {
+
+
+      clickedMatch[0].clicked = true;
+      correctGuesses++;
+
+      if (correctGuesses > bestScore) {
+        bestScore = correctGuesses;
+        this.setState({ bestScore });
+      }
+
+
+      matches.sort(function (a, b) { return 0.5 - Math.random() });
+
+
+      this.setState({ matches });
+      this.setState({ correctGuesses });
+    } else {
+
+
+      clickedMatch[0].clicked = true;
+
+
+      correctGuesses = 0;
+
+
+      bestScore = 12;
+      this.setState({ bestScore });
+
+      for (let i = 0; i < matches.length; i++) {
+        matches[i].clicked = false;
+      }
+
+
+      matches.sort(function (a, b) { return 0.5 - Math.random() });
+
+
+      this.setState({ matches });
+      this.setState({ correctGuesses });
+
+
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+            <Wrapper>
+        
+                <h3 className="scoreSummary instructions">
+                    {this.state.clickMessage}
+                </h3>
+                
+                <h3 className="scoreSummary counter">
+                    Correct Guesses: {this.state.correctGuesses} 
+                    <br />
+                    Best Score: {this.state.bestScore} 
+                </h3>
+
+                {this.state.matches.map(match => (
+                    <MatchCard
+                        setClicked={this.setClicked}
+                        id={match.id}
+                        key={match.id}
+                        image={match.image}
+                    />
+                ))}
+            </Wrapper>
+            </div>
     );
   }
 }
